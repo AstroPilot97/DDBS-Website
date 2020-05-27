5a02eda060bbcdc88abf531803e8d55f6bef5c02
Jakub Dobroń <jakub.dobron8@gmail.com>
2020-05-27 04:15:36 +0200

    Reworked side characters + minor stuff
    Added new character boxes, improved readability, added hover switch on the download button

---
+++ DDBSWebsite/scripts/downloadHover.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', 'graphics/Download pressed.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'graphics/Download.png');
+  }
